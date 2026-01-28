47b3ed3980c798654b3e1a0a521b87384fcace87
fjxfong <fjxfong@gmail.com>
2026-01-29 02:28:59 +0800

    chatbot will be named Psyduck
    renamed main class to Psyduck
    Added Greeting and Goodbye

---
+++ src/main/java/Psyduck.java
@@ -0,0 +1,15 @@
+
+public class Psyduck {
+    public static void main(String[] args) {
+        String greeting = "____________________________________________________________ \n" +
+                "Hello! I'm Psyduck \n" +
+                "What can I do for you? \n" +
+                "____________________________________________________________";
+        String goodbye = "Bye. Hope to see you again soon! \n" +
+                "____________________________________________________________";
+
+
+        System.out.println(greeting);
+        System.out.println(goodbye);
+    }
+}
