5682ab80c343af4dfa392cceaa0bd20c70a64de4
fjxfong <fjxfong@gmail.com>
2026-01-29 02:53:28 +0800

    echo user input and exit on bye

--- src/main/java/Psyduck.java
+++ src/main/java/Psyduck.java
@@ -1,16 +1,33 @@
+import java.util.*;
 
 public class Psyduck {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String greeting = """
                 ____________________________________________________________\s
                 Hello! I'm Psyduck\s
                 What can I do for you?\s
                 ____________________________________________________________""";
+        System.out.println(greeting);
+
+        //waits for input from user
+        //will echo user input to output
+        //will break if user input "bye"
+        while (true) {
+           String input = scanner.nextLine();
+
+           if (input.equals("bye")) {
+               break;
+           }
+
+           System.out.println("____________________________________________________________");
+           System.out.println(" " + input);
+           System.out.println("____________________________________________________________");
+        }
+
         String goodbye = "Bye. Hope to see you again soon! \n" +
                 "____________________________________________________________";
 
-
-        System.out.println(greeting);
         System.out.println(goodbye);
     }
 }
