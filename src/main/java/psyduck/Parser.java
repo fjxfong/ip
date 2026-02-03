994fdb5d3006e4c37dff34164197322dc4d54f01
fjxfong <fjxfong@gmail.com>
2026-02-03 18:14:19 +0800

    Update Parser to return Command objects instead of command type

--- src/main/java/psyduck/Parser.java
+++ src/main/java/psyduck/Parser.java
@@ -1,33 +1,46 @@
 package psyduck;
 
+import psyduck.command.Command;
+import psyduck.command.FindCommand;
+import psyduck.command.ToDoCommand;
+import psyduck.command.DeadlineCommand;
+import psyduck.command.EventCommand;
+import psyduck.command.DeleteCommand;
+import psyduck.command.ExitCommand;
+import psyduck.command.ListCommand;
+import psyduck.command.MarkCommand;
+import psyduck.command.UnmarkCommand;
+
 /**
- * Handles parsing of user input commands.
+ * Handles parsing of user input commands and creates the appropriate Command Object.
  */
 public class Parser {
+
     /**
-     * Parses the user input and returns the corresponding command type.
+     * Parses user input and returns the corresponding Command.
      *
-     * @param input User input string.
-     * @return The command type.
+     * @param input The full user input string.
+     * @return The parsed Command object.
+     * @throws PsyduckException If the command is unknown or input is invalid.
      */
-    public static CommandType parseCommand(String input) {
+    public static Command parse(String input) throws PsyduckException {
         if (input.isEmpty()) {
-            return CommandType.UNKNOWN;
+            throw new PsyduckException("OOPS!!! Please enter a command!");
         }
 
-        String command = input.split(" ")[0].toLowerCase();
+        String commandWord = input.split(" ")[0].toLowerCase();
 
-        return switch (command) {
-            case "bye" -> CommandType.BYE;
-            case "list" -> CommandType.LIST;
-            case "mark" -> CommandType.MARK;
-            case "unmark" -> CommandType.UNMARK;
-            case "delete" -> CommandType.DELETE;
-            case "todo" -> CommandType.TODO;
-            case "deadline" -> CommandType.DEADLINE;
-            case "event" -> CommandType.EVENT;
-            case "find" -> CommandType.FIND_DATE;
-            default -> CommandType.UNKNOWN;
+        return switch (commandWord) {
+            case "bye" -> new ExitCommand();
+            case "list" -> new ListCommand();
+            case "mark" -> new MarkCommand(input);
+            case "unmark" -> new UnmarkCommand(input);
+            case "delete" -> new DeleteCommand(input);
+            case "todo" -> new ToDoCommand(input);
+            case "deadline" -> new DeadlineCommand(input);
+            case "event" -> new EventCommand(input);
+            case "find" -> new FindCommand(input);
+            default -> throw new PsyduckException("OOPS!!! I'm sorry, but I don't know what that means :-(");
         };
     }
 }
